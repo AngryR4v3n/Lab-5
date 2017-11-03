0886ae3e505101756ba8368af591deacdcc3deec
tefabarrio <32182909+tefabarrio@users.noreply.github.com>
2017-11-02 22:21:25 -0600

    Update Cilindrica.java

--- Lab 5/src/Cilindrica.java
+++ Lab 5/src/Cilindrica.java
@@ -9,5 +9,20 @@
  * @author molin
  */
 public class Cilindrica extends Tanque {
-    
+    //variables de instancia
+    double radio;
+    Cilindrica(double radio) {
+        this.radio=radio;
+    }
+    public double calcularVol(double radio, double altura) {
+        double pi= Math.PI;
+        double volumen=pi*radio*radio*altura;
+        return volumen;
+    }
+    /**
+    *@return
+    */
+    public double getRadio() {
+        return radio;
+    }    
 }
