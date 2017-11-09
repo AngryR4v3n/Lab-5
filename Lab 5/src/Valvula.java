f2dc77d986a77488a767ee21104a6156c64f28f2
tefabarrio <32182909+tefabarrio@users.noreply.github.com>
2017-11-08 21:43:03 -0600

    Update Valvula.java

--- Lab 5/src/Valvula.java
+++ Lab 5/src/Valvula.java
@@ -7,7 +7,7 @@
 /**
  * @author Francisco Molina, carne 17050
  * @author Estefania Barrio, carne 17927
- * @version 02/11/2017
+ * @version 08/11/2017
  */
 public class Valvula {
     
@@ -15,7 +15,12 @@ public class Valvula {
     private String idValvula;
     private String municipio;
     private double volumen;
-    
+    /**
+    * @param abierto
+    * @param idValvula
+    * @param municipio
+    * @param volumen
+    */
     public Valvula(boolean abierto, String idValvula, String municipio, double volumen){
         this.abierto=abierto;
         this.idValvula=idValvula;
@@ -34,7 +39,9 @@ public class Valvula {
     public void setCerrado(){
         abierto=false;
     }
-    
+    /**
+    * @return
+    */
     public double getVol(){
         return volumen;
     }
