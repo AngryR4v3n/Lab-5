8994abd9b4c3e620f09bc67ed8864928c7a30e61
tefabarrio <32182909+tefabarrio@users.noreply.github.com>
2017-11-08 20:38:08 -0600

    Update Control.java

--- Lab 5/src/Control.java
+++ Lab 5/src/Control.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 /**
  * @author Francisco Molina, carne 17050
  * @author Estefania Barrio, carne 17927
- * @version 02/11/2017
+ * @version 08/11/2017
  */
 public class Control {
         //public Tanque listaTanque[];
@@ -22,19 +22,41 @@ public class Control {
         tanque=new ArrayList<Tanque>();
         
     }
+    /**
+    * @param altura
+    * @param porcentaje
+    * @param idTanque
+    */
     public void crearCubica(double altura, double porcentaje, String idTanque){
         Cubica x= new Cubica(altura, porcentaje, idTanque);
         tanque.add(x);
     }
+    /**
+    * @param altura
+    * @param porcentaje
+    * @param idTanque
+    * @param apotema
+    * @param perimetro
+    */
     public void crearOrtogonal(double altura, double porcentaje, String idTanque,double apotema,double perimetro){
         Ortogonal x= new Ortogonal(altura,porcentaje,idTanque,apotema,perimetro);
         tanque.add(x);
     }
+    /**
+    * @param altura
+    * @param porcentaje
+    * @param idTanque
+    * @param radio
+    */
     public void crearCilindrica(double altura, double porcentaje, String idTanque,double radio){
         Cilindrica x= new Cilindrica(altura, porcentaje, idTanque,radio);
         tanque.add(x);
     
 }
+    /**
+    * @param id
+    * @return
+    */
     public Tanque busqueda(String id){
         Tanque obj=null;
         for(Tanque c: tanque){
@@ -45,6 +67,12 @@ public class Control {
         
         return obj;
     }
+    /**
+    * @param id
+    * @param idV
+    * @param horas
+    * @return
+    */
     public double abrirVal(String id, String idV, double horas){
         double volActual=0;
             for(Tanque c: tanque){
@@ -57,6 +85,10 @@ public class Control {
             return volActual;
         
     }
+    /**
+    * @param id
+    * @param idV
+    */
     public void cerrarVal(String id, String idV){
         for(Tanque c: tanque){
             if(c.getId().equals(id)){
@@ -65,16 +97,37 @@ public class Control {
             }
         }
     }
+    /**
+    * @param vol
+    * @param obj
+    * @return
+    */
     public double porcentajeTanque(double vol, Tanque obj){
         double porcentaje=(vol/obj.getVolumen())*100;
         return porcentaje;
     }
+    /**
+    * @param vol
+    * @param obj
+    */
     public void setVolumen(double vol, Tanque obj){
         obj.setVolumen(vol);
     }
+    /**
+    * @param nombreT
+    * @param abierto
+    * @param idValvula
+    * @param municipio
+    * @param vol
+    */
     public void crearValvula(String nombreT,boolean abierto,String idValvula, String municipio, double vol){
         this.busqueda(nombreT).crearValvula(abierto,idValvula,municipio,vol);
     }
+    /**
+    * @param elTanque
+    * @param volumenRestante
+    * @return
+    */
     public boolean alerta(Tanque elTanque,double volumenRestante){
         boolean alerta1=false;
         if (this.porcentajeTanque(volumenRestante, elTanque)<=25){
@@ -82,6 +135,11 @@ public class Control {
         }
         return alerta1;
     }
+    /**
+    * @param elTanque
+    * @param volumenRestante
+    * @return
+    */
     public boolean alertaMax(Tanque elTanque,double volumenRestante){
         boolean alerta2=false;
         if (this.porcentajeTanque(volumenRestante, elTanque)<=10){
@@ -89,6 +147,9 @@ public class Control {
         }
         return alerta2;
     }
+    /**
+    * @param name
+    */
     public void cerrado(String name){
         for(Tanque c: tanque){
             if(c.getId().equals(name)){
@@ -96,6 +157,10 @@ public class Control {
             }
         }
     }
+    /**
+    * @param muni
+    * @return
+    */
     public double metrosRegion(String muni){
         double suma=0;
         for(Tanque c: tanque){
@@ -103,6 +168,9 @@ public class Control {
         }
         return suma;
     }
+    /**
+    * @return
+    */
     public String printTodo(){
         String msj="";
         for(Tanque c: tanque){
